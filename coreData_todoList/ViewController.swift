9c79cfdce4229d7bd126c48a45a3b6ee77d15fc7
Sarah Lee <sarahselee223@gmail.com>
2021-02-18 20:36:23 -0800

    create an app

---
+++ coreData_todoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coreData_todoList
+//
+//  Created by Sarah Lee on 2/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
